89b0938a71488cfb8e7d14aaa8ccabf006e85481
detayaa <drugochenko.taya@mail.ru>
2022-09-26 18:52:58 +0700

    1

--- src/main.java
+++ src/main.java
@@ -1,5 +1,25 @@
+import java.time.Year;
+
 public class main {
     public static void main(String[] args) {
 
+        // задание 1
+        checkLeapYear(1988);
+    }
+
+    public static void checkLeapYear(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            System.out.println(year + " високосный год");
+        } else {
+            System.out.println(year + "  не високосный год");
+        }
     }
 }
+
+
+
+
+
+
+
+
